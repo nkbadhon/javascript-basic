95eb032414729ee93d022eaec46f61d2d7e0f8f5
nkbadhon <badhon7574@gmail.com>
2023-12-03 23:16:01 +0600

    while loop

--- compare.js
+++ compare.js
@@ -1,3 +1,5 @@
 var num1 = 60;
 var num12 = 99;
+
+
 console.log(num1 > num12);
---
+++ while.js
@@ -0,0 +1,7 @@
+var roastGiven = 0;
+while (roastGiven < 7) {
+    console.log("Roast Den");
+
+    console.log(roastGiven);
+    roastGiven++;
+}
