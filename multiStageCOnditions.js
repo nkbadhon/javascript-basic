25d06ff4529847a775f4d3199ec1425f3e362228
nkbadhon <badhon7574@gmail.com>
2023-12-03 22:56:32 +0600

    Multiple conditions

--- multiStageCOnditions.js
+++ multiStageCOnditions.js
@@ -2,3 +2,16 @@ var myMoney = 100;
 var danish = 60;
 var banana = 10;
 var toast = 20;
+
+if (danish < myMoney) {
+    console.log("Danish Lagbe");
+}
+else if (banana < myMoney) {
+    console.log("Banana Lagbe");
+}
+else if (toast < myMoney) {
+    console.log("Toast please");
+}
+else {
+    console.log("Kopal e kharap");
+}
